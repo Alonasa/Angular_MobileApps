e155f3c9c731fa04e0b852b621b0f22746d0ccfa
Alona S <alona.s.kry@gmail.com>
2025-12-30 00:04:47 +0000

    added basic product card

---
+++ src/app/recipe-card/recipe-card.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {
+  IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle
+} from "@ionic/angular/standalone";
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-recipe-card',
+  templateUrl: './recipe-card.component.html',
+  styleUrls: ['./recipe-card.component.scss'],
+  imports: [IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, RouterLink]
+})
+export class RecipeCardComponent {
+  @Input() id: number = 0;
+  @Input() title: string = "No products";
+  @Input() image: string=  "img.jpg";
+
+  constructor() { }
+}
